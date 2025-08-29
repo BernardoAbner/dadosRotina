e2d724c394a91a38e6a13124cd9c098a5e4c839b
Bernardo Abner Gomes da Silva Luz <bernardo.luz@findes.org.br>
2025-08-29 17:45:13 -0300

    bug: Correção de logica do codigo e dicionario_planilhas

--- Analise_dados.py
+++ Analise_dados.py
@@ -1,42 +1,52 @@
 import gspread
 import pandas as pd
+import json
 
+
+gc = gspread.oauth()
+email = 'leitor-de-planilhas@dotted-signer-438321-m8.iam.gserviceaccount.com'
+dict_planilhas = {}
+with open("dicionario_planilhas.json", "r") as arquivo:
+    dict_planilhas = json.load(arquivo)
+    print(dict_planilhas)
 
 class planilhas():
-    def carregar_planilha():
-        gc = gspread.oauth()
-        sheet = gc.open('dados-habitos')
-        sheet.share('leitor-de-planilhas@dotted-signer-438321-m8.iam.gserviceaccount.com', perm_type = 'user', role = 'writer')
+
+    def guarda_planilhas(nome_planilha):
+        nome = nome_planilha
+        nome_planilha = {"nome_planilha" : nome, "sheet" : sheet, "worksheet" : worksheet}
+        print (f"Planilha {nome_planilha} foi guardada no dicionário {dict_planilhas}")
+        dict_planilhas[nome] = nome_planilha;
+        return dict_planilhas
+     
+
+    def criar_planilha(nome_planilha):
+        sheet = gc.create(nome_planilha)
         worksheet = sheet.sheet1
-        print ("Planilha compartilhada!")
+        print(f"A Planilha {nome_planilha} foi criada com sucesso!")
+        planilhas.guarda_planilhas(nome_planilha)
+        planilhas.compartilhar_planilha(sheet)
 
-        return gc, worksheet
 
-    def criar_planilhas(self, gc):
-        sheet_bernardo = gc.create("dados-bernardo")
-        sheet_bernardo.share('leitor-de-planilhas@dotted-signer-438321-m8.iam.gserviceaccount.com', perm_type = 'user', role = 'writer')
-        #sheet_bernardo.share('bernardoabnerwsp@gmail.com', perm_type = 'user', role = 'writer')
-        worksheet_bernardo = sheet_bernardo.sheet1
-        print("A planilha do Bernardo foi criada!")
+    def abrir_planilha(sheet, nome_planilha, worksheet):
+        sheet = gc.open(nome_planilha)
+        worksheet = sheet.sheet1
+        print(f"A planilha {nome_planilha} foi aberta!")
+        return worksheet
 
-        sheet_jessyka = gc.create("dados-jessyka")
-        sheet_jessyka.share('leitor-de-planilhas@dotted-signer-438321-m8.iam.gserviceaccount.com', perm_type = 'user', role = 'writer')
-        #sheet_jessyka.share('bernardoabnerwsp@gmail.com', perm_type = 'user', role = 'writer')
-        worksheet_jessyka = sheet_jessyka.sheet1
-        print("A planilha da Jessyka foi criada")
-        print("Lendo dados...")
 
-        return sheet_bernardo, worksheet_bernardo, sheet_jessyka, worksheet_jessyka
-    
+    def compartilhar_planilha(sheet):
+        sheet.share(email, perm_type = 'user', role = 'writer')
+        print(f"Planiilha compartilhada corretamente com o E-mail {email}!")
 
 class manipulacao_dados():
-    def converte_df():
-        dataframe = pd.DataFrame(planilhas.carregando_planilha().worksheet.get_all_records())
+    def converte_df(worksheet):
+        dataframe = pd.DataFrame(worksheet.get_all_records())
         print("Planilha convertida em dataframe!")
         print(dataframe)
 
     def separacao_dados():
-        coluna_usuario = planilhas.carregando_planilha().worksheet.row_values()
+        coluna_usuario = planilhas.worksheet.row_values()
         for usuario in coluna_usuario:
             if usuario == "Bernardo":
                 print("O usuário é o bernardo")
@@ -45,7 +55,23 @@ class manipulacao_dados():
 
 
 if __name__ == "__main__":
-    planilhas.carregar_planilha()
-    planilhas.criar_planilhas()
-    manipulacao_dados.converte_df()
-    manipulacao_dados.separacao_dados()
+
+    '''manipulacao_dados.converte_df()
+    sheet = input("Digite a sheet: ")
+    nome_planilha = input("Digite o nome da planilha: ")
+    worksheet = input("Digite a worksheet: ")
+    planilhas.criar_planilha(nome_planilha)
+    dict_planilhas = planilhas.guarda_planilhas(nome_planilha)
+    print(dict_planilhas)'''
+
+
+    '''planilhas.abrir_planilha(dict_planilhas["dados-habitos"]['sheet'], dict_planilhas["dados-habitos"]['nome_planilha'], dict_planilhas["dados-habitos"]['worksheet'])'''
+
+    manipulacao_dados.converte_df(planilhas.abrir_planilha(dict_planilhas["dados-habitos"]['sheet'], dict_planilhas["dados-habitos"]['nome_planilha'], dict_planilhas["dados-habitos"]['worksheet']))
+
+    with open("dicionario_planilhas.json", "w") as arquivo:
+        json.dump(dict_planilhas, arquivo, indent=4)
+
+    
+
+
